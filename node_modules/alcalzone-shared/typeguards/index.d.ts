4ea0c9540ae5d47be9e95840e65a011bec689e39
Espen Aa Ljosland <ealv@baze.no>
2018-10-21 19:44:22 +0200

    Updated dependencies

---
+++ node_modules/alcalzone-shared/typeguards/index.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Tests whether the given variable is a real object and not an Array
+ * @param it The variable to test
+ */
+export declare function isObject(it: any): it is object;
+/**
+ * Tests whether the given variable is really an Array
+ * @param it The variable to test
+ */
+export declare function isArray(it: any): it is any[];
