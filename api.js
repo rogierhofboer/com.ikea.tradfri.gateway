49d165b51bd533eab6dd9a09460112426e3459f3
rogierhofboer <r.hofboer@ccv.eu>
2023-03-11 22:01:51 +0100

    Migrated to SDKv3 - Fix settings

--- api.js
+++ api.js
@@ -1,40 +1,13 @@
 'use strict';
-const Homey = require('homey');
-module.exports = [
-    {
-        method: 'POST',
-        path: '/discover/',
-        fn: async (args, callback) => {
-            try {
-                let result = await this.homey.app.discover();
-                return callback(null, result);
-            } catch (err) {
-                return callback(err);
-            }
-        }
-    },
-    {
-        method: 'POST',
-        path: '/authenticate/',
-        fn: async (args, callback) => {
-            try {
-                let result = await this.homey.app.authenticate(args.body.name, args.body.secret);
-                return callback(null,result);
-            } catch (err) {
-                return callback(err);
-            }
-        } 
-    },
-    {
-        method: 'POST',
-        path: '/connect/',
-        fn: async (args, callback) => {
-            try {
-                let result = await this.homey.app.connect();
-                return callback(null, result);
-            } catch (err) {
-                return callback(err);
-            }
-        }
-    }
-];
+module.exports = {
+  async discover({ homey, body}) {
+    return await homey.app.discover();
+  },
+  
+  async authenticate({ homey, body}) {
+    return await homey.app.authenticate(body.name, body.secret);
+  },
+  async connect({ homey, body}) {
+    return await homey.app.connect();
+  }
+};
